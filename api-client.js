30ed6e02e0a1f8c0becdf5c20e0b039b968ef33f
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-07-19 10:18:02 +0200

    Update api-client.js

--- api-client.js
+++ api-client.js
@@ -1,12 +1,18 @@
-const apiUrl = 'http://localhost:3000'
+const apiUrl = 'http://localhost:3000/'
 
 
 const getData = async () => {
   try {
-        const res = await fetch (apiUrl);
+        const res = await fetch (apiUrl,{
+          method: 'GET',
+          body: JSON.stringify(data),
+          headers: {
+      'Content-type': 'application/json'
+    },})
         const data = await res.json();
         console.log(data)
     }
    catch (err){
     console.log(err)
 }}    
+getData()
