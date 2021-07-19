075594e18ff34e4f9003d1532e71875e9a9283d1
Randy Duran <randyduranperez@gmail.com>
2021-07-20 01:17:17 +0200

    updated

--- api-client.js
+++ api-client.js
@@ -1,12 +1,51 @@
-const apiUrl = 'http://localhost:3000'
+const apiUrl = 'http://localhost:3000/'
 
-
-const getData = async () => {
-  try {
-        const res = await fetch (apiUrl);
+const getTodos = async () => {
+    try {
+        const res = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            },
+        });
         const data = await res.json();
-        console.log(data)
+        return data;
     }
-   catch (err){
-    console.log(err)
-}}    
+    catch (err) {
+        console.log(err)
+    }
+}
+const postTodo = async (body) => {
+  try {
+      await fetch(apiUrl, {
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: {
+              'Content-type': 'application/json'
+          },
+      });
+  }
+  catch (err) {
+      console.log(err)
+  }
+}
+
+const deleteTodo = async (id) => {
+  try {
+      const res = await fetch(apiUrl + id, {
+          method: 'DELETE',
+          headers: {
+              'Content-type': 'application/json'
+          },
+      });
+      const data = await res.json();
+      return data;
+  }
+  catch (err) {
+      console.log(err)
+  }
+}
+//  postData({ description: 'werken' });
+console.log(getTodos());
+
+// deleteData('80e60210-eb96-4c00-ba98-bae8d1368987')
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const input = document.querySelector('.todo-input')
+const input = document.querySelector('#todo-input')
 const list =document.querySelector('#list')
 const button = document.querySelector('#button-input')
 
@@ -6,42 +6,57 @@ const button = document.querySelector('#button-input')
 
 function addTask(event){
     event.preventDefault();
-    console.log('working!')
 
     const newDiv = document.createElement('div')
+    newDiv.classList.add("container");
 
     const newLI = document.createElement('li');
     newLI.innerText= input.value;
+    newLI.classList.add('item')
     newDiv.appendChild(newLI);
+ 
+    const checkButton = document.createElement('button');
+    checkButton.innerHTML = '✓';
+    newDiv.appendChild(checkButton);
+    checkButton.classList.add("check-btn")
 
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class = "fas fa-trash"></i>';
     newDiv.appendChild(deleteButton) ;
-    checkButton.classList.add("delete-btn")
+    deleteButton.classList.add("delete-btn")
 
-    const checkButton = document.createElement('button');
-    checkButton.innerHTLM = '<i class = "fas fa-check"></i>';
-    newDiv.appendChild(checkButton);
-    checkButton.classList.add("check-btn")
+   
 
     list.appendChild(newDiv);
     input.value="";
 
 }
 
-button.addEventListener('click', aadTask);
-list.addEventListener('click', deleteCheck);
+
 
 function deleteCheck(e){
     console.log(e.target)
     const task = e.target;
     if (task.classList[0] === 'delete-btn'){
-        let task = task.parentElement;
+        const todo = task.parentElement;
+        todo.remove();
     }
   
-    if (task.classList[0] === 'check-btn'){
-        let task = task.parentElement;
-        task.classList.toggle('checked'  )
+    else if (task.classList[0] === 'check-btn'){
+        const todo  = task.parentElement;
+        todo.classList.toggle('checked')
     }
 }
+button.addEventListener('click', addTask);
+list.addEventListener('click', deleteCheck);
 
+getTodos()
+
+const data = {description: "buy oatmeal", done: false};
+ fetch(apiUrl, {
+  method: "POST",
+  body: JSON.stringify(data),
+  headers: {
+      "Content-Type": "application/json",
+  },
+});
